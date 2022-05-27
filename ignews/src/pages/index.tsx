ad0de09a4c4e05e44bd990f8a126d2d8e60d5f98
Diego Rosa <diego.rosa@synvia.com>
2022-05-27 08:50:51 -0300

    fixed: home page

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,17 +1,28 @@
 import Head from 'next/head';
 import React from 'react';
 
+import styles from './home.module.scss';
+
 const Home: React.FC = () => {
   return (
    <React.Fragment>
         <Head>
           <title>
-            Inicio | id.news
+           Home | id.news
           </title>
         </Head>
-        <h1 >
-          Ig News
-        </h1>
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, Welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publications <br/>
+              <span>$for $9.90 month</span>
+            </p>
+          </section>
+
+          <img src="/images/avatar.svg" alt="Girl coding"/>
+        </main>
    </React.Fragment>
   )
 }
